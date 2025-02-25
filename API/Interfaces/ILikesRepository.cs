68ab26706b10fdb8f6b0a20244c8a1bcc3424f95
G4S-web <carlo@wms-it.net>
2025-02-25 15:30:10 +0200

    Endo of Section 18

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -16,6 +16,4 @@ public interface ILikesRepository
     void DeleteLike(UserLike like);
 
     void AddLike(UserLike like);
-
-    Task<bool> SaveChanges();
 }
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -10,7 +10,6 @@ public interface IUserRepository
 {
     void Update(AppUser user);
 
-    Task<bool> SaveAllAsync();
     Task<IEnumerable<AppUser>> GetUserAsync();
 
     Task<AppUser?> GetUserByIdAsync(int id);
